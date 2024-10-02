b54dec9b9cdd5c0d57c95a133f284be5f089c4aa
OliFryser <oliver-1999@live.dk>
2024-10-02 19:19:13 +0200

    removed sprites

--- GameProgramming24/Exercise4/Engine/Engine/MyEngine.h
+++ GameProgramming24/Exercise4/Engine/Engine/MyEngine.h
@@ -28,10 +28,6 @@ namespace MyEngine {
 		void Update(float);
 		void Render();
 
-		float GetFPS() const { return 1.0 / time_elapsed.count(); }
-		float GetTimeElapsedMs() const { return 1000 * time_elapsed.count(); }
-		float GetTimeComputationMs() const { return 1000 * time_computation.count(); }
-
 		glm::vec2 GetScreenSize() const;
 
 		int GetFrame() const { return frame; }
@@ -42,11 +38,6 @@ namespace MyEngine {
 	private:
 		std::shared_ptr<GameObject> _root;
 
-		std::chrono::time_point<std::chrono::steady_clock>	time_start;
-		std::chrono::time_point<std::chrono::steady_clock>	time_end;
-		std::chrono::time_point<std::chrono::steady_clock>	time_end_computation;
-		std::chrono::duration<double>						time_elapsed;
-		std::chrono::duration<double>						time_computation;
 		int frame;
 		float time;
 
